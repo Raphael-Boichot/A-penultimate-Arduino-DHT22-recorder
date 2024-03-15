e505cde413c5542f327f0d74f904dceb3027042c
Raphaël BOICHOT <63796796+Raphael-Boichot@users.noreply.github.com>
2024-03-15 16:55:56 +0100

    Add files via upload

--- Codes/DHT22_SD_Set_RTC/DHT22_SD_Set_RTC.ino
+++ Codes/DHT22_SD_Set_RTC/DHT22_SD_Set_RTC.ino
@@ -11,26 +11,22 @@ void setup() {
   pinMode(GREEN_LED, OUTPUT);
   if (!rtc.begin()) {
     digitalWrite(RED_LED, 1);
-    Serial.println("Module RTC non retrouvé !");
+    Serial.println("// RTC Module not working !");
     while (1)
       ;
   }
+  Serial.println("// Injecting Time/Date from PC to the working module");
   rtc.adjust(DateTime(__DATE__, __TIME__));
   digitalWrite(GREEN_LED, 1);
 }
 
 void loop() {
   DateTime date = rtc.now();
-  Serial.print(date.day());
-  Serial.print("/");
-  Serial.print(date.month());
-  Serial.print("/");
-  Serial.print(date.year());
-  Serial.print(" ");
-  Serial.print(date.hour());
-  Serial.print(":");
-  Serial.print(date.minute());
-  Serial.print(":");
-  Serial.println(date.second());
+  // this part was made with ChatGPT
+  char formattedDate[20];  // Table to store the formatted date
+  // Date format YYYY-MM-DD HH:MM:SS
+  sprintf(formattedDate, "Date/Time: %04d-%02d-%02d %02d:%02d:%02d", date.year(), date.month(), date.day(), date.hour(), date.minute(), date.second());
+  // end of part made with ChatGPT
+  Serial.println(formattedDate);
   delay(1000);
 }
