18f0e2aa32a3f951d9330aaa116b83da8ae69cd4
Dr. BOICHOT <raphael.boichot@gmail.com>
2026-02-17 15:14:56 +0100

    Add files via upload

--- Codes/DHT22_SD_Set_RTC/DHT22_SD_Set_RTC.ino
+++ Codes/DHT22_SD_Set_RTC/DHT22_SD_Set_RTC.ino
@@ -9,15 +9,44 @@ void setup() {
   Serial.begin(115200);
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
+
   if (!rtc.begin()) {
     digitalWrite(RED_LED, 1);
-    Serial.println("// RTC Module not working !");
+    Serial.println(F("// RTC Module not working !"));
     while (1)
       ;
   }
-  Serial.println("// Injecting Time/Date from PC to the working module");
-  rtc.adjust(DateTime(__DATE__, __TIME__));
-  digitalWrite(GREEN_LED, 1);
+
+  // 1. Capture the "New Time" (Compilation time)
+  DateTime pcTime = DateTime(__DATE__, __TIME__);
+
+  // 2. Capture the "Current RTC Time" (Before sync)
+  DateTime rtcTime = rtc.now();
+
+  // 3. Calculate Drift
+  // unixtime() gives total seconds since Jan 1st 1970
+  long driftSeconds = pcTime.unixtime() - rtcTime.unixtime();
+
+  Serial.println(F("--- RTC SYNC REPORT ---"));
+  Serial.print(F("RTC Time: "));
+  Serial.println(rtcTime.timestamp());
+  Serial.print(F("PC Time:  "));
+  Serial.println(pcTime.timestamp());
+
+  Serial.print(F("Drift detected: "));
+  Serial.print(driftSeconds);
+  Serial.println(F(" seconds"));
+
+  if (abs(driftSeconds) > 2) {
+    Serial.println(F("// Drift > 2s: Injecting PC time..."));
+    rtc.adjust(pcTime);
+    digitalWrite(GREEN_LED, 1);
+    delay(1000);
+    digitalWrite(GREEN_LED, 0);
+  } else {
+    Serial.println(F("// Time is already accurate. No sync needed."));
+  }
+  Serial.println(F("-----------------------"));
 }
 
 void loop() {
